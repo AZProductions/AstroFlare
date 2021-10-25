a61842221115a09fd81030f567c60eb44286ebcf
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-10-25 17:42:35 +0200

    Start of the AstroFlare project. [Created AstroFlare.sln and AstroFlare.Compiler]
    - Created **AstroFlare.sln**.
    - Created **AstroFlare.Compiler**.

---
+++ AstroFlare.Compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AstroFlare.Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
