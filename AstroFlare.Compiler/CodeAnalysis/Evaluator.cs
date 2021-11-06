236eeacd508861919b7def18766941313334ae49
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-11-06 17:31:57 +0100

    Replace switch with operator lookup. [Main]

--- AstroFlare.Compiler/CodeAnalysis/Evaluator.cs
+++ AstroFlare.Compiler/CodeAnalysis/Evaluator.cs
@@ -28,7 +28,7 @@ namespace AstroFlare.Compiler.CodeAnalysis
             {
                 var operand = EvaluateExpression(u.Operand);
 
-                switch (u.OperatorKind)
+                switch (u.Op.Kind)
                 {
                     case BoundUnaryOperatorKind.Identity:
                         return (int)operand;
@@ -37,7 +37,7 @@ namespace AstroFlare.Compiler.CodeAnalysis
                     case BoundUnaryOperatorKind.LogicalNegation:
                         return !(bool)operand;
                     default:
-                        throw new Exception($"Unexpected unary operator {u.OperatorKind}");
+                        throw new Exception($"Unexpected unary operator {u.Op}");
                 }
             }
 
@@ -46,7 +46,7 @@ namespace AstroFlare.Compiler.CodeAnalysis
                 var left = EvaluateExpression(b.Left);
                 var right = EvaluateExpression(b.Right);
 
-                switch (b.OperatorKind)
+                switch (b.Op.Kind)
                 {
                     case BoundBinaryOperatorKind.Addition:
                         return (int)left + (int)right;
@@ -61,7 +61,7 @@ namespace AstroFlare.Compiler.CodeAnalysis
                     case BoundBinaryOperatorKind.LogicalOr:
                         return (bool)left || (bool)right;
                     default:
-                        throw new Exception($"Unexpected binary operator {b.OperatorKind}");
+                        throw new Exception($"Unexpected binary operator {b.Op}");
                 }
             }
 
