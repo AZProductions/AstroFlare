f5acb50d17262d69c01713083f583fb495ad2a8a
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-11-09 18:32:25 +0100

    Added tests to ensure we honor unary and binary precedence. [Main]

--- AstroFlare.Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ AstroFlare.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -55,11 +55,64 @@ namespace AstroFlare.Tests.CodeAnalysis.Syntax
                     e.AssertToken(SyntaxKind.IdentifierToken, "a");
                     e.AssertToken(op1, op1Text);
                     e.AssertNode(SyntaxKind.BinaryExpression);
-                    e.AssertNode(SyntaxKind.NameExpression);                    
+                    e.AssertNode(SyntaxKind.NameExpression);
                     e.AssertToken(SyntaxKind.IdentifierToken, "b");
                     e.AssertToken(op2, op2Text);
                     e.AssertNode(SyntaxKind.NameExpression);
                     e.AssertToken(SyntaxKind.IdentifierToken, "c");
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUnaryOperatorPairsData))]
+        public void Parser_UnaryExpression_HonorsPrecedences(SyntaxKind unaryKind, SyntaxKind binaryKind)
+        {
+            var unaryPrecedence = SyntaxFacts.GetUnaryOperatorPrecedence(unaryKind);
+            var binaryPrecedence = SyntaxFacts.GetBinaryOperatorPrecedence(binaryKind);
+            var unaryText = SyntaxFacts.GetText(unaryKind);
+            var binaryText = SyntaxFacts.GetText(binaryKind);
+            var text = $"{unaryText} a {binaryText} b";
+            var expression = SyntaxTree.Parse(text).Root;
+
+            if (unaryPrecedence >= binaryPrecedence)
+            {
+                //   binary
+                //   /    \
+                // unary   b
+                //   |
+                //   a
+
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.UnaryExpression);
+                    e.AssertToken(unaryKind, unaryText);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    e.AssertToken(binaryKind, binaryText);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                }
+            }
+            else
+            {
+                //  unary
+                //    |
+                //  binary
+                //  /   \
+                // a     b
+
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.UnaryExpression);
+                    e.AssertToken(unaryKind, unaryText);
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    e.AssertToken(binaryKind, binaryText);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
                 }
             }
         }
@@ -71,6 +124,17 @@ namespace AstroFlare.Tests.CodeAnalysis.Syntax
                 foreach (var op2 in SyntaxFacts.GetBinaryOperatorKinds())
                 {
                     yield return new object[] { op1, op2 };
+                }
+            }
+        }
+
+        public static IEnumerable<object[]> GetUnaryOperatorPairsData()
+        {
+            foreach (var unary in SyntaxFacts.GetUnaryOperatorKinds())
+            {
+                foreach (var binary in SyntaxFacts.GetBinaryOperatorKinds())
+                {
+                    yield return new object[] { unary, binary };
                 }
             }
         }
