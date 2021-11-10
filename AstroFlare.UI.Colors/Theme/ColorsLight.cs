f496fa5d8b9633929d556ffcc76d0b72ec16f6e7
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-11-10 12:36:26 +0100

    Added all color themes. [UI]

---
+++ AstroFlare.UI.Colors/Theme/ColorsLight.cs
@@ -0,0 +1,40 @@
+namespace AstroFlare.UI.Colors
+{
+    public class ColorsLight
+    {
+        public static readonly string LightPrimaryColor = "#f3fbff";
+        public static readonly string PrimaryColor = "#326cf3";
+        public static readonly string DarkPrimaryColor = "#326cf3";
+        public static readonly string LightDangerColor = "#fff6f7";
+        public static readonly string DangerColor = "#db3340";
+        public static readonly string DarkDangerColor = "#db3340";
+        public static readonly string LightWarningColor = "#fffcf5";
+        public static readonly string WarningColor = "#e9af20";
+        public static readonly string DarkWarningColor = "#e9af20";
+        public static readonly string LightInfoColor = "#f1fdff";
+        public static readonly string InfoColor = "#00bcd4";
+        public static readonly string DarkInfoColor = "#00bcd4";
+        public static readonly string LightSuccessColor = "#f3fff6";
+        public static readonly string SuccessColor = "#2db84d";
+        public static readonly string DarkSuccessColor = "#2db84d";
+        public static readonly string PrimaryTextColor = "#212121";
+        public static readonly string SecondaryTextColor = "#757575";
+        public static readonly string ThirdlyTextColor = "#bdbdbd";
+        public static readonly string ReverseTextColor = "#212121";
+        public static readonly string TextIconColor = "White";
+        public static readonly string BorderColor = "#e0e0e0";
+        public static readonly string SecondaryBorderColor = "#757575";
+        public static readonly string BackgroundColor = "#eeeeee";
+        public static readonly string RegionColor = "#ffffff";
+        public static readonly string SecondaryRegionColor = "#eeeeee";
+        public static readonly string ThirdlyRegionColor = "White";
+        public static readonly string TitleColor = "#326cf3";
+        public static readonly string SecondaryTitleColor = "#326cf3";
+        public static readonly string DefaultColor = "White";
+        public static readonly string DarkDefaultColor = "#f5f5f5";
+        public static readonly string AccentColor = "#f8491e";
+        public static readonly string DarkAccentColor = "#f8491e";
+        public static readonly string DarkMaskColor = "#20000000";
+        public static readonly string DarkOpacityColor = "#40000000";
+    }
+}
