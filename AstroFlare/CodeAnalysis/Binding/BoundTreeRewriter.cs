69c6fe4836edeaa74bba784d1a9036056b228c6d
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-11-15 15:27:59 +0100

    Improved Repl. [Main]

--- AstroFlare/CodeAnalysis/Binding/BoundTreeRewriter.cs
+++ AstroFlare/CodeAnalysis/Binding/BoundTreeRewriter.cs
@@ -118,7 +118,7 @@ namespace AstroFlare.Compiler.CodeAnalysis.Binding
             if (condition == node.Condition)
                 return node;
 
-            return new BoundConditionalGotoStatement(node.Label, condition, node.JumpIfFalse);
+            return new BoundConditionalGotoStatement(node.Label, condition, node.JumpIfTrue);
         }
 
         protected virtual BoundStatement RewriteExpressionStatement(BoundExpressionStatement node)
--- AstroFlare/CodeAnalysis/Lowering/Lowerer.cs
+++ AstroFlare/CodeAnalysis/Lowering/Lowerer.cs
@@ -64,7 +64,7 @@ namespace AstroFlare.Compiler.CodeAnalysis.Lowering
                 // <then>  
                 // end:
                 var endLabel = GenerateLabel();
-                var gotoFalse = new BoundConditionalGotoStatement(endLabel, node.Condition, true);
+                var gotoFalse = new BoundConditionalGotoStatement(endLabel, node.Condition, false);
                 var endLabelStatement = new BoundLabelStatement(endLabel);
                 var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(gotoFalse, node.ThenStatement, endLabelStatement));
                 return RewriteStatement(result);
@@ -88,7 +88,7 @@ namespace AstroFlare.Compiler.CodeAnalysis.Lowering
                 var elseLabel = GenerateLabel();
                 var endLabel = GenerateLabel();
 
-                var gotoFalse = new BoundConditionalGotoStatement(elseLabel, node.Condition, true);
+                var gotoFalse = new BoundConditionalGotoStatement(elseLabel, node.Condition, false);
                 var gotoEndStatement = new BoundGotoStatement(endLabel);
                 var elseLabelStatement = new BoundLabelStatement(elseLabel);
                 var endLabelStatement = new BoundLabelStatement(endLabel);
@@ -126,7 +126,7 @@ namespace AstroFlare.Compiler.CodeAnalysis.Lowering
             var gotoCheck = new BoundGotoStatement(checkLabel);
             var continueLabelStatement = new BoundLabelStatement(continueLabel);
             var checkLabelStatement = new BoundLabelStatement(checkLabel);
-            var gotoTrue = new BoundConditionalGotoStatement(continueLabel, node.Condition, false);
+            var gotoTrue = new BoundConditionalGotoStatement(continueLabel, node.Condition);
             var endLabelStatement = new BoundLabelStatement(endLabel);
 
             var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(
@@ -150,7 +150,8 @@ namespace AstroFlare.Compiler.CodeAnalysis.Lowering
             //
             // {
             //      var <var> = <lower>
-            //      while (<var> <= <upper>)
+            //      let upperBound = <upper>
+            //      while (<var> <= upperBound)
             //      {
             //          <body>
             //          <var> = <var> + 1
@@ -159,25 +160,31 @@ namespace AstroFlare.Compiler.CodeAnalysis.Lowering
 
             var variableDeclaration = new BoundVariableDeclaration(node.Variable, node.LowerBound);
             var variableExpression = new BoundVariableExpression(node.Variable);
+            var upperBoundSymbol = new VariableSymbol("upperBound", true, typeof(int));
+            var upperBoundDeclaration = new BoundVariableDeclaration(upperBoundSymbol, node.UpperBound);
             var condition = new BoundBinaryExpression(
                 variableExpression,
                 BoundBinaryOperator.Bind(SyntaxKind.LessOrEqualsToken, typeof(int), typeof(int)),
-                node.UpperBound
-            );            
+                new BoundVariableExpression(upperBoundSymbol)
+            );
             var increment = new BoundExpressionStatement(
                 new BoundAssignmentExpression(
                     node.Variable,
                     new BoundBinaryExpression(
-                            variableExpression,
-                            BoundBinaryOperator.Bind(SyntaxKind.PlusToken, typeof(int), typeof(int)),
-                            new BoundLiteralExpression(1)
+                        variableExpression,
+                        BoundBinaryOperator.Bind(SyntaxKind.PlusToken, typeof(int), typeof(int)),
+                        new BoundLiteralExpression(1)
                     )
                 )
             );
             var whileBody = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(node.Body, increment));
             var whileStatement = new BoundWhileStatement(condition, whileBody);
-            var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(variableDeclaration, whileStatement));
-     
+            var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(
+                variableDeclaration,
+                upperBoundDeclaration,
+                whileStatement
+            ));
+
             return RewriteStatement(result);
         }
     }
